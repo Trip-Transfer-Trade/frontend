17e8b0bbdfd2b473537c5830ed30b134576b36a3
inhooinu <inhooinu@gmail.com>
2025-02-25 09:14:48 +0900

    feat: update BackNavigation component and apply to exchange pages (#8)

--- src/components/BackNavigation.jsx
+++ src/components/BackNavigation.jsx
@@ -1,13 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
 
-export default function BackHeader() {
+export default function BackNavigation({ text = "" }) {
   const navigate = useNavigate();
 
   return (
     <div className="w-full flex items-center p-2 bg-white">
-      <button onClick={() => navigate(-1)} className="p-2">
+      <button onClick={() => navigate(-1)} className="p-2 flex items-center">
         <ArrowLeft size={24} />
+        <span className="ml-2 text-lg font-bold">{text}</span>
       </button>
     </div>
   );
--- src/pages/ExchangePage/CurrencyExchangePage.jsx
+++ src/pages/ExchangePage/CurrencyExchangePage.jsx
@@ -1,6 +1,9 @@
+import BackNavigation from "../../components/BackNavigation";
+
 export default function CurrencyExchangePage() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      <BackNavigation text="환전하기" />
       <h1>Currency Exchange Page</h1>
     </div>
   );
