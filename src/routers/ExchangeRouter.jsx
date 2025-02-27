5da7ba92ab863dbaf6f7106649a1c36b0ed2a077
jeongIN <shjin4667@gmail.com>
2025-02-28 08:30:12 +0900

    stock trading page

--- src/routers/ExchangeRouter.jsx
+++ src/routers/ExchangeRouter.jsx
@@ -1,7 +1,7 @@
 import ExchangeMainPage from "../pages/ExchangePage/ExchangeMainPage";
 import MyWalletPage from "../pages/ExchangePage/MyWalletPage";
-import LiveExchangeRatesPage from "../pages/ExchangePage/LiveExchangeRatesPage";
-import LiveExchangeRateDetailsPage from "../pages/ExchangePage/LiveExchangeRateDetailsPage";
+// import LiveExchangeRatesPage from "../pages/ExchangePage/LiveExchangeRatesPage";
+// import LiveExchangeRateDetailsPage from "../pages/ExchangePage/LiveExchangeRateDetailsPage";
 import CurrencyExchangePage from "../pages/ExchangePage/CurrencyExchangePage";
 import ExchangeCompletePage from "../pages/ExchangePage/ExchangeCompletePage";
 
@@ -10,8 +10,8 @@ const ExchangeRouter = {
   children: [
     { index: true, element: <ExchangeMainPage /> },
     { path: "wallet", element: <MyWalletPage /> },
-    { path: "rates", element: <LiveExchangeRatesPage /> },
-    { path: "rates/:id", element: <LiveExchangeRateDetailsPage /> },
+    // { path: "rates", element: <LiveExchangeRatesPage /> },
+    // { path: "rates/:id", element: <LiveExchangeRateDetailsPage /> },
     { path: "currency-exchange", element: <CurrencyExchangePage /> },
     { path: "complete", element: <ExchangeCompletePage /> },
   ],
