7a58eb67ac7c3dd2068a99da3134e0e192a3bfa5
inhooinu <inhooinu@gmail.com>
2025-02-28 13:23:55 +0900

    feat: modify exchange list components (#8)

--- src/components/ExchangeComponent/ExchangeRateList.jsx
+++ src/components/ExchangeComponent/ExchangeRateList.jsx
@@ -7,7 +7,7 @@ export default function ExchangeRateList({
   const visibleData = showLimited ? exchangeRates.slice(0, 3) : exchangeRates; // 조건에 따라 3개 or 전체 표시
 
   return (
-    <div className="flex-grow w-full mx-auto">
+    <div className="w-full mx-auto">
       {visibleData.map((item, index) => (
         <ExchangeRateItem key={index} {...item} />
       ))}
