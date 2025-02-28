552e886826ebfca040ad82791afff887ffd358ff
0321minji <0321minji@ewhain.net>
2025-02-28 15:35:05 +0900

    Feat : create exchange method select modal(#30)

--- src/routers/TripRouter.jsx
+++ src/routers/TripRouter.jsx
@@ -2,14 +2,15 @@ import FailedPage from "../pages/MyTripPage/TripGoal/FailedPage";
 import SuccessPage from "../pages/MyTripPage/TripGoal/SuccessPage";
 import InfoPage from "../pages/MyTripPage/TripGoal/InfoPage";
 import SuccessDetailPage from "../pages/MyTripPage/TripGoal/SuccessDetailPage";
-
+import ExchangeMethodPage from "../pages/ExchangePage/ExchangeMethodPage";
 const TripRouter = {
   path: "/trip",
   children: [
     { path:"failed", element : <FailedPage/>},
     { path:"success", element : <SuccessPage />},
     { path:"info", element : <InfoPage />},
-    { path:"success/detail", element : <SuccessDetailPage />}
+    { path:"success/detail", element : <SuccessDetailPage />},
+    { path:"exchange", element : <ExchangeMethodPage />}
   ],
 };
 
