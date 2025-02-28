b9048e37117e8c0a6edad3421fea69748bbcf002
inhooinu <inhooinu@gmail.com>
2025-02-28 11:23:54 +0900

    feat: install libraries and configure router for exchange pages (#8)

--- src/routers/ExchangeRouter.jsx
+++ src/routers/ExchangeRouter.jsx
@@ -1,19 +1,21 @@
 import ExchangeMainPage from "../pages/ExchangePage/ExchangeMainPage";
 import MyWalletPage from "../pages/ExchangePage/MyWalletPage";
-// import LiveExchangeRatesPage from "../pages/ExchangePage/LiveExchangeRatesPage";
-// import LiveExchangeRateDetailsPage from "../pages/ExchangePage/LiveExchangeRateDetailsPage";
+import ExchangeRatesPage from "../pages/ExchangePage/ExchangeRatesPage";
+import ExchangeRateDetailsPage from "../pages/ExchangePage/ExchangeRateDetailsPage";
 import CurrencyExchangePage from "../pages/ExchangePage/CurrencyExchangePage";
 import ExchangeCompletePage from "../pages/ExchangePage/ExchangeCompletePage";
+import Test from "../pages/ExchangePage/Test";
 
 const ExchangeRouter = {
   path: "/exchange",
   children: [
     { index: true, element: <ExchangeMainPage /> },
     { path: "wallet", element: <MyWalletPage /> },
-    // { path: "rates", element: <LiveExchangeRatesPage /> },
-    // { path: "rates/:id", element: <LiveExchangeRateDetailsPage /> },
-    { path: "currency-exchange", element: <CurrencyExchangePage /> },
+    { path: "rates", element: <ExchangeRatesPage /> },
+    { path: "rates/:id", element: <ExchangeRateDetailsPage /> },
+    { path: "currency", element: <CurrencyExchangePage /> },
     { path: "complete", element: <ExchangeCompletePage /> },
+    { path: "test", element: <Test /> },
   ],
 };
 
