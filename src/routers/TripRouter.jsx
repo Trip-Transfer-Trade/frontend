c5caf2e7f3b58721cf6a075b570718290c1823ea
MinYeong-mkk <kmy26377538@gmail.com>
2025-02-28 15:43:33 +0900

    portfolio page

--- src/routers/TripRouter.jsx
+++ src/routers/TripRouter.jsx
@@ -2,14 +2,23 @@ import FailedPage from "../pages/MyTripPage/FailedPage";
 import SuccessPage from "../pages/MyTripPage/SuccessPage";
 import InfoPage from "../pages/MyTripPage/InfoPage";
 import SuccessDetailPage from "../pages/MyTripPage/SuccessDetailPage";
-
+import PortfolioPage from "../pages/PortfolioPage/PortfolioPage";
+import TripListPage from "../pages/MyTripPage/TripListPage";
+import Home from "../pages/PortfolioPage/page";
+import TripEditPage from "../pages/MyTripPage/TripEditpage";
+import RankingPage from "../pages/PortfolioPage/RankingPage";
 const TripRouter = {
   path: "/trip",
   children: [
     { path:"failed", element : <FailedPage/>},
     { path:"success", element : <SuccessPage />},
     { path:"info", element : <InfoPage />},
-    { path:"success/detail", element : <SuccessDetailPage />}
+    { path:"success/detail", element : <SuccessDetailPage />},
+    { path:"portfolio", element : <PortfolioPage/>},
+    { path:"list", element : <TripListPage/>},
+    { path:"test", element : <Home/>},
+    { path:"edit", element : <TripEditPage/>},
+    { path:"portfolio/rank", element : <RankingPage/>}
   ],
 };
 
