32b7ca6e6a39606ccc47ff17c9e2c082832001bc
0321minji <0321minji@ewhain.net>
2025-02-28 17:04:25 +0900

    hotfix : modify image route in pages

--- src/pages/SignUpPage/SignupCompletePage.jsx
+++ src/pages/SignUpPage/SignupCompletePage.jsx
@@ -1,13 +1,13 @@
 import NextConfirmButton from "../../components/NextConfirmButton";
 
-import complete from "../../assets/images/complete.svg";
+// import complete from "../../assets/images/complete.svg";
 
 export default function SignupCompletePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col items-center justify-center flex-grow">
         <div className="p-6">
-          <img src={complete} alt="complete" />
+          <img src="/assets/images/complete.svg" alt="complete" />
         </div>
         <h2 className="text-xl font-bold">회원가입 완료</h2>
       </div>
