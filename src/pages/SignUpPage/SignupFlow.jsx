d13c759adc13ed72d8e45de4d193e3673bbb49e9
MinYeong-mkk <kmy26377538@gmail.com>
2025-03-04 10:33:21 +0900

    fix: Update image path(#51)

--- src/pages/SignUpPage/SignupFlow.jsx
+++ src/pages/SignUpPage/SignupFlow.jsx
@@ -94,7 +94,7 @@ export default function SignupFlow() {
           <div className="flex flex-col items-center justify-center min-h-screen">
             <div className="flex flex-col items-center justify-center flex-grow">
               <div className="p-6">
-                <img src="/src/assets/images/complete.svg" alt="complete" />
+                <img src="/assets/images/complete.svg" alt="complete" />
               </div>
               <h2 className="text-xl font-bold">회원가입 완료</h2>
             </div>
