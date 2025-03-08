d7d5021cafbdb1de4ff6daae25b937218229234d
MinYeong-mkk <kmy26377538@gmail.com>
2025-03-08 11:04:40 +0900

    feat:목표 조회, 생성 back 연결 및 ui 수정(#63)

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -1,11 +1,11 @@
 import Layout from "../layout/Layout";
 
 import MainPage from "../pages/MainPage/MainPage";
-import TripMainPage from "../pages/MyTripPage/TripMainPage";
 import ExchangeMainPage from "../pages/ExchangePage/ExchangeMainPage";
 import UserPage from "../pages/UserPage/UserPage";
 
 import HomeRouter from "./HomeRouter";
+import TripMainPage from "../pages/MyTripPage/TripMainPage";
 
 const RootRouter = {
   path: "/",
