54116e10454a9cfc85b2bff647c3c3e3a0fe4380
cie10 <leesky0075@dgu.ac.kr>
2025-03-08 14:36:21 +0900

    feat: Implement Authprovider(#84)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/router";
+import { AuthProvider } from "./\bcontext/AuthProvider";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return(
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
 export default App;
