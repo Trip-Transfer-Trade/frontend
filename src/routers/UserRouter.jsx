5839febe8e0caa0d172546b39918b2e1326fb180
cie10 <leesky0075@dgu.ac.kr>
2025-03-09 13:46:37 +0900

    feat: logout, fetch userInfo(#92)

--- src/routers/UserRouter.jsx
+++ src/routers/UserRouter.jsx
@@ -3,12 +3,12 @@ import TransferRecipientPage from "../pages/UserPage/TransferRecipientPage";
 import TransferAmountPage from "../pages/UserPage/TransferAmountPage";
 import TransferConfirmPage from "../pages/UserPage/TransferConfirmPage";
 import HistoryPage from "../pages/UserPage/HistoryPage";
+import ProfilePage from "../pages/UserPage/ProfilePage";
 
 
 const UserRouter = {
   path: "/mypage",
   children: [
-
     {
       path: "transfer",
       children: [
@@ -24,12 +24,16 @@ const UserRouter = {
           path: "confirm",
           element: <TransferConfirmPage />, // 최종 확인 페이지
         },
-        { path: "history", 
-          element: <HistoryPage /> 
+        {
+          path: "history",
+          element: <HistoryPage />,
         },
       ],
     },
-
+    {
+      path: "profile",
+      element: <ProfilePage />, // 독립적인 프로필 페이지
+    },
   ],
 };
 
