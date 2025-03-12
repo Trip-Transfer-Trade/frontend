4bbfa562e225983d6233b259b0d5815211b6e1f0
0321minji <0321minji@ewhain.net>
2025-03-12 13:06:53 +0900

    Feat : create exchange success page(#120)

---
+++ src/components/ExchangeComponent/ExchangCompleteResultCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const ExchangeCompleteResultCard = ({
+    title,
+    principal,
+    exchangeRate,
+    exchangeAmount,
+    icon, // 아이콘 URL (선택사항)
+    bgColor = "bg-blue-50",       // 카드 배경색
+    subBgColor = "bg-blue-100",    // 환전 금액 영역 배경색
+  }) => {
+    return (
+      <div className={`w-full max-w-md ${bgColor} rounded-xl p-4 mb-2`}>
+        <div className="flex items-center mb-4">
+          <div className="w-8 h-8 bg-gray-300 rounded-full mr-3">
+            {icon && (
+              <img
+                src={icon}
+                alt="icon"
+                className="w-full h-full object-contain"
+              />
+            )}
+          </div>
+          <span className="font-medium text-[14px]">{title}</span>
+        </div>
+  
+        <div className="px-2">
+            <div className="flex justify-between text-[12px]">
+            <span className="text-gray-700 text-[12px]">원금</span>
+            <span className="font-medium">{principal}</span>
+            </div>
+  
+            <div className="flex justify-between mb-4 text-[12px]">
+            <span className="text-gray-700 ">적용 환율</span>
+            <span className="font-medium text-red-500">{exchangeRate}</span>
+            </div>
+        </div>
+        <div className={`${subBgColor} rounded-lg p-3 flex justify-between text-[14px]`} >
+          <span className="font-medium">환전 금액</span>
+          <span className="font-medium">{exchangeAmount}</span>
+        </div>
+      </div>
+    );
+  };
+
+export default ExchangeCompleteResultCard;
