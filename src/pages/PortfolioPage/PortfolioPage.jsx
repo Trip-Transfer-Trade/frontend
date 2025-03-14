88003c9c2c880f96c71a625812cff4f4e3761fde
cie10 <leesky0075@dgu.ac.kr>
2025-03-14 18:19:47 +0900

    edit url

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import { fetchTripById } from "../../redux/tripSlice";
 import { fetchAssets } from "../../redux/assetsSlice";
-import AssetsList from "../../components/Portfolio/AssetList";
+import AssetsList from "../../components/portfolio/AssetList";
 import BackNavigation from "../../components/BackNavigation";
 import PortfolioChart from "./PortfolioChart";
 import PortfolioAccount from "./PortfolioAccount";
