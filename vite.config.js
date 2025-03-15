bcd14829265d319955f35e51bb76299106e31f52
cie10 <leesky0075@dgu.ac.kr>
2025-03-16 05:10:53 +0900

    edit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     react(), 
     tailwindcss(),
@@ -49,8 +50,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://triptransfertrade.shop/api',
-        baseURL: "http://localhost:8080/api",
+        target: 'https://triptransfertrade.shop/api',
+        // baseURL: "http://localhost:8080/api",
         changeOrigin: true,
         secure: false,
       }
