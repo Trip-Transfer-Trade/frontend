b1403b024639f473760d917243efc1ff9438cb07
jeongIN <shjin4667@gmail.com>
2025-03-17 00:33:10 +0900

    fix: add goal (#208)

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -28,11 +28,13 @@ export default function UserPage() {
     apiClient
       .get("/accounts/count")
       .then((response) => {
-        const count = response.data;
+        let count = response.data;
         if(count === 1){
-          count -= 1;
+          count = 0;
+          setCount(count);
+        } else {
+          setCount(count - 1);
         }
-        setCount(response.data);
         console.log("진행 중 목표 : " + count);
       })
       .catch((err) => {
